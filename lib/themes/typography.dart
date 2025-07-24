dab402294d2a294d64149c4cca4872e45353c2f5
Ammu J Shajan <a0000070.oth@tatamotors.com>
2025-07-24 14:34:38 +0530

    Custom text theme added in typography

--- lib/themes/typography.dart
+++ lib/themes/typography.dart
@@ -38,3 +38,100 @@ class TuxTypography {
     bodyMedium: GoogleFonts.saira(fontSize: 14, letterSpacing: 0),
   );
 }
+
+/* [CustomTextTheme] all custom text style define here */
+extension TuxTextTheme on TextTheme {
+  /* DisplayLarge */
+  TextStyle? get displayLarge600 =>
+      displayLarge?.copyWith(fontWeight: FontWeight.w600);
+  /* DisplayLarge */
+  TextStyle? get displayLarge500 =>
+      displayLarge?.copyWith(fontWeight: FontWeight.w500);
+  /* DisplayLarge */
+  TextStyle? get displayLarge400 =>
+      displayLarge?.copyWith(fontWeight: FontWeight.w400);
+  /* DisplaySmall */
+  TextStyle? get displaySmall500 =>
+      displaySmall?.copyWith(fontWeight: FontWeight.w500);
+
+  /* DisplaySmall */
+  TextStyle? get displaySmall400 =>
+      displaySmall?.copyWith(fontWeight: FontWeight.w400);
+
+  /* HeadLineLarge */
+  TextStyle? get headlineLarge600 =>
+      headlineLarge?.copyWith(fontWeight: FontWeight.w600);
+  TextStyle? get headlineLarge500 =>
+      headlineLarge?.copyWith(fontWeight: FontWeight.w500);
+  TextStyle? get headlineLarge400 =>
+      headlineLarge?.copyWith(fontWeight: FontWeight.w400);
+  /* HeadLineMedium */
+  TextStyle? get headlineMedium600 =>
+      headlineMedium?.copyWith(fontWeight: FontWeight.w600);
+
+  TextStyle? get headlineMedium500 =>
+      headlineMedium?.copyWith(fontWeight: FontWeight.w500);
+
+  TextStyle? get headlineMedium400 =>
+      headlineMedium?.copyWith(fontWeight: FontWeight.w400);
+  TextStyle? get headlineSmall400 =>
+      headlineSmall?.copyWith(fontWeight: FontWeight.w400);
+  /* HeadLineSmall */
+  TextStyle? get headlineSmall500 =>
+      headlineSmall?.copyWith(fontWeight: FontWeight.w500);
+  TextStyle? get headlineExtraSmall600 => headlineSmall?.copyWith(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    height: 1.2,
+  );
+  TextStyle? get headlineExtraSmall500 => headlineSmall?.copyWith(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    height: 1.2,
+  );
+  TextStyle? get headlineExtraSmall400 => headlineSmall?.copyWith(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    height: 1.2,
+  );
+  TextStyle? get headlineExtraSmall300 => headlineSmall?.copyWith(
+    fontSize: 20,
+    fontWeight: FontWeight.w300,
+    height: 1.2,
+  );
+  /* LabelLarge */
+  TextStyle? get labelLarge600 =>
+      labelLarge?.copyWith(fontWeight: FontWeight.w600);
+  TextStyle? get labelLarge500 =>
+      labelLarge?.copyWith(fontWeight: FontWeight.w500);
+  TextStyle? get labelLarge400 =>
+      labelLarge?.copyWith(fontWeight: FontWeight.w400);
+  /* LabelMedium */
+  TextStyle? get labelMedium600 =>
+      labelMedium?.copyWith(fontWeight: FontWeight.w600);
+  TextStyle? get labelMedium500 =>
+      labelMedium?.copyWith(fontWeight: FontWeight.w500);
+  TextStyle? get labelMedium400 =>
+      labelMedium?.copyWith(fontWeight: FontWeight.w400);
+  /* Bodylarge */
+  TextStyle? get bodyLarge600 =>
+      bodyLarge?.copyWith(fontWeight: FontWeight.w600);
+  TextStyle? get bodyLarge500 =>
+      bodyLarge?.copyWith(fontWeight: FontWeight.w500);
+  TextStyle? get bodyLarge400 =>
+      bodyLarge?.copyWith(fontWeight: FontWeight.w400);
+  /* BodyMedium */
+  TextStyle? get bodyMedium600 =>
+      bodyMedium?.copyWith(fontWeight: FontWeight.w600);
+  TextStyle? get bodyMedium500 =>
+      bodyMedium?.copyWith(fontWeight: FontWeight.w500);
+  TextStyle? get bodyMedium400 =>
+      bodyMedium?.copyWith(fontWeight: FontWeight.w400);
+
+  /* ButtonLabel */
+  TextStyle? get buttonLabel => labelLarge?.copyWith(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    height: 1.5,
+  );
+}
