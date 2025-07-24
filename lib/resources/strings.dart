98a5f5b667339caa2f2a474b4bbbbbf8b048dc18
Ammu J Shajan <a0000070.oth@tatamotors.com>
2025-07-24 14:34:08 +0530

    String file for the entire application

---
+++ lib/resources/strings.dart
@@ -0,0 +1,8 @@
+// Strings for the application, used in various widgets.
+class Strings {
+  Strings._();
+  static const String homeLabel = 'Home';
+  static const String aboutLabel = 'About Me';
+  static const String projectsLabel = 'Projects';
+  static const String contactLabel = 'Contact';
+}
