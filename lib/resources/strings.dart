a56d66f79e28d178f5fe9b525925f1129d640b71
Ammu J Shajan <a0000070.oth@tatamotors.com>
2025-07-29 08:58:16 +0530

    Name, job and description - added in Home page

--- lib/resources/strings.dart
+++ lib/resources/strings.dart
@@ -5,4 +5,8 @@ class Strings {
   static const String aboutLabel = 'About Me';
   static const String projectsLabel = 'Projects';
   static const String contactLabel = 'Contact';
+  static const String name = 'Ammu J Shajan';
+  static const String role = 'Senior Software Engineer';
+  static const String description =
+      'Im a front end developer with 5 years of crafting clean code, fixing bugs before coffee kicks in, and leading teams without breaking apps (or spirits). Loves clean UIs and chaotic commit messages.';
 }
