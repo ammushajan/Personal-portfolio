ed342561703c4e371ce6a677f93f50a31dcd39c8
Ammu J Shajan <a0000070.oth@tatamotors.com>
2025-07-29 08:58:16 +0530

    Name, job and description - added in Home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,14 +1,60 @@
 import 'package:flutter/material.dart';
 
+import 'package:personal_portfolio/themes/colors.dart';
+import 'package:personal_portfolio/resources/strings.dart';
+import 'package:personal_portfolio/themes/typography.dart';
+import 'package:personal_portfolio/widgets/common/base_layout.dart';
+
 ///[HomePage] is a widget that represents the home screen of the application.
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Home Page')),
-      body: Center(child: Text('Welcome to the Home Page!')),
+    Size screenSize = MediaQuery.of(context).size;
+    return BaseLayout(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Text(
+              Strings.name,
+              style: Theme.of(
+                context,
+              ).textTheme.displayLarge500?.copyWith(color: AppColors.white),
+            ),
+            SizedBox(height: 20),
+            ShaderMask(
+              blendMode: BlendMode.srcIn,
+              shaderCallback:
+                  (bounds) => LinearGradient(
+                    colors: [AppColors.white, AppColors.black],
+                  ).createShader(
+                    Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+                  ),
+              child: Text(
+                Strings.role,
+                style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                  color: AppColors.white,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            SizedBox(
+              width: screenSize.width * 0.4,
+              child: Text(
+                Strings.description,
+                textAlign: TextAlign.center,
+                style: Theme.of(
+                  context,
+                ).textTheme.headlineSmall?.copyWith(color: AppColors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
