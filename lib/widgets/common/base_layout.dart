6a663cce9e3a11b728ca6559d23db78c928b9eaf
Ammu J Shajan <a0000070.oth@tatamotors.com>
2025-08-04 19:37:58 +0530

    Base layout and header - gradient color fixes

--- lib/widgets/common/base_layout.dart
+++ lib/widgets/common/base_layout.dart
@@ -7,23 +7,21 @@ import 'package:personal_portfolio/widgets/common/header.dart';
 /// It includes an app bar and a body that can be customized with different widgets.
 class BaseLayout extends StatelessWidget {
   final Widget child;
-  const BaseLayout({super.key, required this.child});
+  final List<Color>? gradientColors;
+  const BaseLayout({super.key, required this.child, this.gradientColors});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Header(),
-      extendBodyBehindAppBar: true,
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
-            colors: [
-              AppColors.purplePizzazz,
-              AppColors.cyan,
-              AppColors.blackRock,
-            ],
+            colors:
+                gradientColors ??
+                [AppColors.purplePizzazz, AppColors.cyan, AppColors.blackRock],
             stops: const [0.0, 0.8, 1.0],
           ),
         ),
