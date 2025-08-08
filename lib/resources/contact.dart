ff83cdf2e7121eacd6e93721507f071234064e35
Ammu J Shajan <a0000070.oth@tatamotors.com>
2025-08-08 12:23:51 +0530

    LinkedIn and Github details added

--- lib/resources/contact.dart
+++ lib/resources/contact.dart
@@ -4,6 +4,7 @@ class Contact {
   static const String emailId = 'ammushajanj@gmail.com';
   static const String phoneNumber = '+91 8220468574';
   static const String location = 'Kerala, India';
-  static const String linkedInUrl = 'linkedin.com/in/ammu-j-shajan-730914169';
+  static const String linkedInUrl =
+      'https://linkedin.com/in/ammu-j-shajan-730914169';
   static const String githubUrl = 'https://github.com/ammushajan';
 }
