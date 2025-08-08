a26e5abd7a3b3ec4f878daa1526eebc279ae545f
Ammu J Shajan <a0000070.oth@tatamotors.com>
2025-08-08 10:38:41 +0530

    URL path strategy added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
 import 'package:ammu_portfolio/app.dart';
 
 void main() {
+  usePathUrlStrategy();
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const PortfoilioApp());
 }
